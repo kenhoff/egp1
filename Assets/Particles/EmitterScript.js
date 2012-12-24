0ea06b07a40611b436a4f50d15c62b35bb3c1473
Ken Hoff <thekenhoff@gmail.com>
2012-12-24 12:59:34 -0700

    worked on beams some more

--- Assets/Particles/EmitterScript.js
+++ Assets/Particles/EmitterScript.js
@@ -5,6 +5,7 @@ private var particles_left : int;
 public var prefab : Transform;
 public var rate : float;
 private var accum_time : float;
+public var particles_root : Transform;
 
 function Start () {
 	particles_left = particles;
@@ -12,7 +13,8 @@ function Start () {
 
 function Update () {
 	if ((accum_time > rate) && (particles_left > 0)) {
-			Instantiate (prefab, transform.position + Vector3(Random.value, Random.value, 0), Quaternion.identity);
+			var clone = Instantiate (prefab, transform.position + Vector3(Random.value, Random.value, 0), Quaternion.identity);
+			clone.parent = particles_root;
 			particles_left -= 1;
 			accum_time = 0;
 	}
--- Assets/PlayerControl.js
+++ Assets/PlayerControl.js
@@ -1,14 +1,15 @@
 #pragma strict
 
-private var horizontal_speed = 4;
+private var horizontal_speed = 10;
 private var	max_horizontal_speed = 5;
 private var jump_upward_velocity = 4;
 private var min_jump_velocity = 3;
 
-private var terrain_raycast = (1<<9);
+private var terrain_raycast_mask = (1<<9);
+private var beam_raycast_mask = (1<<11);
 
-public var HeatRayPrefab : Transform;
-public var FreezeRayPrefab : Transform;
+public var HeatRayObject : GameObject;
+public var FreezeRayObject : GameObject;
 
 function Start () {
 
@@ -24,7 +25,7 @@ function FixedUpdate () {
 
 	var horizontal_input = Input.GetAxis("Horizontal") * horizontal_speed;
 	var speed = -Vector3.right * horizontal_input;
-	rigidbody.velocity += speed;
+	rigidbody.AddForce(speed);
 
 	// curb velocity
 
@@ -41,7 +42,7 @@ function FixedUpdate () {
 
 	// jump from ground
 
-    if (Input.GetButton("Jump") && Physics.Raycast (transform.position, -Vector3.up, 1.1, terrain_raycast)) {
+    if (Input.GetButton("Jump") && Physics.Raycast (transform.position, -Vector3.up, 1.1, terrain_raycast_mask)) {
 		rigidbody.velocity = rigidbody.velocity + (Vector3.up * jump_upward_velocity);
 		Debug.DrawRay(transform.position, -Vector3.up * 1);
 	}
@@ -65,6 +66,10 @@ function FixedUpdate () {
 	else {
 		transform.eulerAngles.y = 180;
 	}
+
+
+	// beams
+
 	if (Input.GetButton("Fire1")) {
 		var pos = Input.mousePosition;
 		var center = Vector3(Screen.width/2, Screen.height/2, 0);
@@ -75,8 +80,35 @@ function FixedUpdate () {
 
 		var hit : RaycastHit;
 		Physics.Raycast(transform.position, dir, hit);
-		Debug.DrawRay(transform.position, dir.normalized * hit.distance);
-		// var beam = Instantiate(HeatRayPrefab)
+		var end_beam_location = transform.position + (dir.normalized * hit.distance);
+		Debug.DrawLine(transform.position, end_beam_location);
+
+
+		var angle_of_mousecursor = Mathf.Rad2Deg * Mathf.Atan2( (Input.mousePosition.y - Screen.width/2) , (Input.mousePosition.x - Screen.height/2));
+		Debug.Log(angle_of_mousecursor);
+
+
+		// var capsule_hit : RaycastHit;
+		// if  (Physics.CapsuleCast (transform.position, end_beam_location, 1, Vector3.zero, capsule_hit)) {
+		// 	Debug.Log("hit!");
+		// 	Debug.Log(capsule_hit.collider);
+		// }
+		if (!HeatRayObject.activeSelf) {
+			HeatRayObject.SetActive(true);
+		}
+		HeatRayObject.transform.position = transform.position;
+		HeatRayObject.transform.localScale = Vector3(1, hit.distance, 1);
+		HeatRayObject.transform.eulerAngles = Vector3(0, 0, -angle_of_mousecursor + 85);
+		HeatRayObject.transform.position = transform.position + ((end_beam_location- transform.position) / 2);
+
 	}
+	else {
+		HeatRayObject.SetActive(false);
+		FreezeRayObject.SetActive(false);
+	}
+
+}
+
+function DrawPlayerBeam () {
 
 }
